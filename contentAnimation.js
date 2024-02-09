a2f5265a2fcaa069b3ab64c48758470faea5bc4e
+ <+>
2024-02-09 09:54:39 -0600

    updating website intro

---
+++ contentAnimation.js
@@ -0,0 +1,1 @@
+// contentAnimation.js module //
--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+// Any global scope variables will be added here //
+const headerAnimation = document.querySelector('.headerAnimation');
+const headerAnimation2 = document.querySelector('.header');
+// Logo link is written in JS for quality purposes to ensure undisturbed positioning //
+function clickLogo() {
+    const logoLink = document.querySelector('#flatIronLink');
+    
+    logoLink.addEventListener('click', (e) => {
+        const anchorTag = document.createElement('a');
+        anchorTag.href = 'https://flatironschool.com'
+        anchorTag.setAttribute('target', '_blank', "title=flatironschool.com")
+        window.open(anchorTag)
+    })
+} 
 
-document.querySelector('body').appendChild(h2);
+clickLogo()
+
+function setIntroAnimations() {
+    const logoAnimation = document.querySelector('.logoAnimation');
+    const logoAnimation2 = document.querySelector('.logoAnimationTwo');
+
+     document.addEventListener('DOMContentLoaded', () => {
+        headerAnimation.style.display = 'grid';
+        headerAnimation.style.animation = 'loadingHeader 2s forwards';
+     })
+
+     headerAnimation.addEventListener('animationend', () => {
+        logoAnimation.style.display = 'grid';
+        logoAnimation.style.position = 'static'
+        logoAnimation.style.animation = 'dropLogo 1s ease-out'
+     }) 
+
+     logoAnimation.addEventListener('animationend', () => {
+      logoAnimation2.style.display = 'grid';
+      logoAnimation2.style.animation = 'spinLogo 400ms forwards'
+   }) 
+} 
+
+setIntroAnimations() 
+
+
+  
+  
+  
